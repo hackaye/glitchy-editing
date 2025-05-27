3ee2f97b4f022319d568940c19e3e1c8ae5df392
Sue Smith <sue@benormal.info>
2025-05-27 14:39:48 +0100

    Delete .devcontainer/_helpers directory

--- .devcontainer/_helpers/welcome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script runs when your project starts in the Codespace!
-
-printf "\nHi! This project replicates some of the UI features in the Glitch editor.\n\nThe scripts will attempt to build and preview your site on startup.\n\nWhen your preview opens, use the 🔎 Split button at the bottom to see your website and code side by side.\n\nYou can close the Terminal while you work.. 🐝\n\n"

b979aa11bcb225c765850dd0dc42428c5cba2fd2
Sue Smith <sue@benormal.info>
2025-06-12 10:46:55 +0100

    Update run.sh

--- _helpers/run.sh
+++ _helpers/run.sh
@@ -1,8 +1,6 @@
 # We're making some guesses about the package scripts lolol
 
-OOPS="\n🥴 hmm maybe check your package.json...?\n\n"
+OOPS="\n🤬 hmm maybe check your package.json...?\n\n"
 
 npm install || printf "${OOPS}"
 npm run start || printf "${OOPS}"
-npm run build || printf "${OOPS}"
-npm run serve || printf "${OOPS}"

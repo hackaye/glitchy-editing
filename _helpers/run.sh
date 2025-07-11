4a8f71d6776e739659c93f8750d49c49bb823b49
Sue Smith <sue@benormal.info>
2025-07-11 11:38:40 +0100

    Update run.sh

--- _helpers/run.sh
+++ _helpers/run.sh
@@ -1,6 +1,14 @@
 # We're making some guesses about the package scripts lolol
 
-OOPS="\n🤬 hmm maybe check your package.json...?\n\n"
+if [ ! -f package.json ]; then
+
+printf "\n🤔 No package.json found – try the Open button instead!\n\n"    
+
+else 
+
+OOPS="\n🤬 hmm maybe check what's in your package.json scripts and try "npm run script-name"?\n\n"
 
 npm install || printf "${OOPS}"
 npm run start || printf "${OOPS}"
+
+fi
